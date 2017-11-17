2005d95d71b53674fb70c8f5eb27fa722b37718d
santeeno <alexsanteeno@gmail.com>
2017-11-17 10:15:37 +0200

    Initial commit

---
+++ registration.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @author Rebus
+ */
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Rebus_PlannerCart',
+    __DIR__
+);
+?>
