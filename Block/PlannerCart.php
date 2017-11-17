4de984aff795ef9ba92386252f878243e8e719f6
santeeno <alexsanteeno@gmail.com>
2017-11-17 14:47:21 +0200

    Module created

---
+++ Block/PlannerCart.php
@@ -0,0 +1,12 @@
+<?php
+
+
+namespace Rebus\PlannerCart\Block;
+
+class PlannerCart extends \Magento\Framework\View\Element\Template
+{
+    public function _prepareLayout()
+    {
+        return parent::_prepareLayout();
+    }
+}
--- Controller/Index/Index.php
+++ Controller/Index/Index.php
@@ -1,63 +1,22 @@
 <?php
 namespace Rebus\PlannerCart\Controller\Index;
-use Magento\Framework\App\Action\Context;
+
 
 class Index extends \Magento\Framework\App\Action\Action
 {
-    protected $resultPageFactory;
-    protected $formKey;
-    protected $request;
-    protected $cart;
-    protected $product;
-    protected $productRepository;
-    protected $resourceModel;
-
-
-
-    public function __construct(Context $context,
-                                \Magento\Framework\View\Result\PageFactory $resultPageFactory,
-                                \Magento\Catalog\Model\Product $product,
-                                \Magento\Framework\Data\Form\FormKey $formKey,
-                                \Magento\Checkout\Model\Cart $cart,
-                                \Magento\Catalog\Api\ProductRepositoryInterface $productRepository,
-                                \Magento\Catalog\Model\ResourceModel\Product $resourceModel
-    ){
-        $this->resultPageFactory = $resultPageFactory;
-        $this->cart = $cart;
-        $this->product = $product;
-        $this->formKey = $formKey;
-        $this->productRepository = $productRepository;
-        $this->resourceModel = $resourceModel;
-        parent::__construct($context);
-    }
-    public function execute(){
-
-        $post = $this->getRequest()->getParams();
-
-
-
-        if(!empty($post['command'] && $post['command'] == 'basket')){
-            foreach ($post['items'] as $product_item){
-                $params = array(
-                    'qty' => $product_item['quantity']
-                );
-                $product_sku = str_replace(' ', '',$product_item['id']);
-
-                $productId = $this->resourceModel->getIdBySku($product_sku);
-                if(!empty($productId)){
-                    $_product = $this->productRepository->get($product_sku);
-                    if($_product){
-                        $this->cart->addProduct($_product,$params);
-                    }
-                }
-            }
-        }
-       if($this->cart->save()){
-           echo json_encode('success');
-       };
-
-
+    protected $_pageFactory;
+    public function __construct(
+        \Magento\Framework\App\Action\Context $context,
+        \Magento\Framework\View\Result\PageFactory $pageFactory)
+    {
+        $this->_pageFactory = $pageFactory;
+        return parent::__construct($context);
     }
 
+    public function execute()
+    {
+        $resultPage = $this->_pageFactory->create();
+        $resultPage->getConfig()->getTitle()->set('3DPlanner');
+        return $resultPage;
+    }
 }
-?>
